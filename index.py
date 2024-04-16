d96359b08fbaa357a13ac7e7b87a0e1dddf0c36d
a52290451 <cuchitrin555@hotmail.com>
2024-04-15 21:32:43 -0500

    feat: corrección de librerías

--- index.py
+++ index.py
@@ -4,5 +4,5 @@ from src import init_app
 configuration = config['development']
 app = init_app(configuration)
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080)
